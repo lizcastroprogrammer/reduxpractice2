e5d1b797e507d3541f5f6a7aa7d737fdcedc4c89
lizcastroprogrammer <elizabethjcastro42@gmail.com>
2018-06-24 15:58:23 -0700

    added api functionality and reducers

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'd25db0f97e1a7e43269b12e4008f95f2';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
